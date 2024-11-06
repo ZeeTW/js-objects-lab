45d3276bc8119ed2d982feaa352920fb569785a5
ZeeTW <zainab.alafoo@hotmail.com>
2024-11-06 23:32:08 +0300

    Exercise 9

--- script.js
+++ script.js
@@ -115,3 +115,15 @@ Solve Exercise 8 here:
 game.party.forEach((member) => {
   console.log(member.name)
 })
+
+/*
+Exercise 9
+1. Can you print out all the starter Pokémon from the `pokemon` array?
+2. Think about how you can identify a starter Pokémon and then log their names.
+
+
+Solve Exercise 9 here:
+*/
+
+const starterPokemons = pokemon.filter((pokemon) => pokemon.starter === true)
+console.log(starterPokemons)
